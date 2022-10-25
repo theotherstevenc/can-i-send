9c6f46f75d9752ea0016c3f97225daffab451061
stevencollins <s.collins@salesforce.com>
2022-10-25 14:16:50 -0700

    fix amp live preview

--- public/js/main.js
+++ public/js/main.js
@@ -88,10 +88,9 @@ htmlEditor.on('change', htmlPreview)
 function ampPreview(){
   session.editor.amp = ampEditor.getValue()
   sessionStorage.setItem('_SESSION_amp', session.editor.amp)
-	var preview = document.querySelector('.ampframe').contentDocument;
-	preview.open();
-	preview.write(ampEditor.getValue());
-	preview.close();
+  var preview = document.querySelector('.ampframe')
+  const html = ampEditor.getValue()
+  preview.src = `data:text/html;charset=utf-8, ${encodeURIComponent(html)}`
 }
 ampEditor.on('change', ampPreview)
 
