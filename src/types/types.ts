9e42ad461c8a07aadc65d98d1e3d44d0fd461ba1
stevencollins <s.collins@salesforce.com>
2024-11-26 14:14:27 -0800

    Consolidate types and interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -30,3 +30,60 @@ export type OptionCheckBoxProps = {
   setWordWrap: (value: boolean) => void
   setPreventThreading: (value: boolean) => void
 }
+
+export interface EditorContextProps {
+  html: string
+  setHtml: React.Dispatch<React.SetStateAction<string>>
+  originalHtml: string
+  setOriginalHtml: React.Dispatch<React.SetStateAction<string>>
+  text: string
+  setText: React.Dispatch<React.SetStateAction<string>>
+  amp: string
+  setAmp: React.Dispatch<React.SetStateAction<string>>
+  activeEditor: string
+  setActiveEditor: React.Dispatch<React.SetStateAction<string>>
+  editorSizes: number[]
+  setEditorSizes: React.Dispatch<React.SetStateAction<number[]>>
+  sizes: number[]
+  setSizes: React.Dispatch<React.SetStateAction<number[]>>
+  preventThreading: boolean
+  setPreventThreading: React.Dispatch<React.SetStateAction<boolean>>
+  minifyHTML: boolean
+  setMinifyHTML: React.Dispatch<React.SetStateAction<boolean>>
+  wordWrap: boolean
+  setWordWrap: React.Dispatch<React.SetStateAction<boolean>>
+  email: string[]
+  setEmail: React.Dispatch<React.SetStateAction<string[]>>
+  subject: string
+  setSubject: React.Dispatch<React.SetStateAction<string>>
+  loading: boolean
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  alertState: {
+    message: string
+    severity: 'error' | 'success'
+    open: boolean
+  }
+  setAlertState: React.Dispatch<
+    React.SetStateAction<{
+      message: string
+      severity: 'error' | 'success'
+      open: boolean
+    }>
+  >
+  senderSettings: {
+    host: string
+    port: string
+    user: string
+    pass: string
+    from: string
+  }
+  setSenderSettings: React.Dispatch<
+    React.SetStateAction<{
+      host: string
+      port: string
+      user: string
+      pass: string
+      from: string
+    }>
+  >
+}
