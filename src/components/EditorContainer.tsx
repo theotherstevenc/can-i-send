40da1c978f28dc904693eee8e9d6e3ff01482eed
theotherstevenc <steven.winston.collins@gmail.com>
2025-04-26 09:55:37 -0700

    fix: update EditorContainer to conditionally apply class for empty file state

--- src/components/EditorContainer.tsx
+++ src/components/EditorContainer.tsx
@@ -2,11 +2,15 @@ import { Box } from '@mui/material'
 import EditorWorkspacePreview from './EditorWorkspacePreview'
 import Split from 'react-split'
 import EditorWorkingFiles from './EditorWorkingFiles'
+import { useEditorContext } from '../context/EditorContext'
 
 const EditorContainer = () => {
+  const { files } = useEditorContext()
+  const isFilesEmpty = files.length === 0 || files.length === undefined
+
   return (
     <Split className='split-component' sizes={[10, 90]}>
-      <Box>
+      <Box className={isFilesEmpty ? 'no-working-files' : ''}>
         <EditorWorkingFiles />
       </Box>
       <Box sx={{ flexGrow: 1 }}>
