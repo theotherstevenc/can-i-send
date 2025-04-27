aa2aeca442620573bbb65deab194e7422228ca8c
theotherstevenc <steven.winston.collins@gmail.com>
2025-04-26 20:50:57 -0700

    feat: add toggle button labels and initialize hideWorkingFiles state in EditorContext

--- src/context/EditorContext.tsx
+++ src/context/EditorContext.tsx
@@ -14,7 +14,7 @@ export const EditorProvider: React.FC<{ children: React.ReactNode }> = ({ childr
   const [workingFileName, setWorkingFileName] = useState<string>('')
   const [triggerFetch, setTriggerFetch] = useState<boolean>(false)
   const [files, setFiles] = useState<WorkingFile[]>([])
-  const [hideWorkingFiles, setHideWorkingFiles] = useState<boolean>(false)
+  const [hideWorkingFiles, setHideWorkingFiles] = useState<boolean>(true)
 
   return (
     <EditorContext.Provider
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
 export const BUTTON_VARIANT_OUTLINED = 'outlined'
 export const BUTTON_VARIANT_CONTAINED = 'contained'
+export const TOGGLE_BUTTON_SHOW_PROJECTS = 'Show Projects'
+export const TOGGLE_BUTTON_HIDE_PROJECTS = 'Hide Projects'
