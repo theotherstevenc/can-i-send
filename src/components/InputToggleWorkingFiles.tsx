76e9a0ab13f57da34ba9bb134cda333b15627d45
theotherstevenc <steven.winston.collins@gmail.com>
2025-04-26 17:16:48 -0700

    feat: add InputToggleWorkingFiles component for toggling visibility of working files

---
+++ src/components/InputToggleWorkingFiles.tsx
@@ -0,0 +1,24 @@
+import { Tooltip } from '@mui/material'
+import ToggleOffIcon from '@mui/icons-material/ToggleOff'
+import ToggleOnIcon from '@mui/icons-material/ToggleOn'
+import { StyledIconButton } from './InputIconButton'
+import { useEditorContext } from '../context/EditorContext'
+
+const InputToggleWorkingFiles = () => {
+  const { hideWorkingFiles, setHideWorkingFiles } = useEditorContext()
+
+  const handleOpen = () => {
+    setHideWorkingFiles(!hideWorkingFiles)
+  }
+
+  return (
+    <>
+      <Tooltip title='Show/Hide Working Files'>
+        <StyledIconButton onClick={handleOpen} aria-label='Rename Project'>
+          {hideWorkingFiles ? <ToggleOffIcon /> : <ToggleOnIcon />}
+        </StyledIconButton>
+      </Tooltip>
+    </>
+  )
+}
+export default InputToggleWorkingFiles
