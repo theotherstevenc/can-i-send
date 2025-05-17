5e8a06f7ba857d412643fdfed3b8054ad471e0cb
theotherstevenc <steven.winston.collins@gmail.com>
2025-05-16 22:05:09 -0700

    feat: add dialog for login/logout functionality

--- src/components/Authenticator.tsx
+++ src/components/Authenticator.tsx
@@ -1,38 +1,53 @@
 import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { StyledIconButton } from './InputIconButton'
-import { BTN_LABEL_LOGIN, BTN_LABEL_LOGOUT } from '../utils/constants'
+import { BTN_LABEL_CANCEL, BTN_LABEL_LOGIN, BTN_LABEL_LOGOUT, BTN_LABEL_OK, LABEL_CLOSE } from '../utils/constants'
 import LogoutIcon from '@mui/icons-material/Logout'
 import LoginIcon from '@mui/icons-material/Login'
-import { Tooltip } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, TextField, Tooltip } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+
 import usePersistentAuth from '../utils/usePersistentAuth'
+import { useState } from 'react'
 
 export const Authenticator = () => {
-  const loginEmail = ''
-  const loginPassword = ''
-
-  const [isAuth, setIsAuth] = usePersistentAuth('isLoggedIn', false)
-
   const auth = getAuth()
 
-  const handleClick = async () => {
-    if (isAuth) {
-      try {
-        await signOut(auth)
-        setIsAuth(false)
-        console.log('User signed out')
-      } catch (error) {
-        console.error('Error signing out:', error)
-      }
-    }
+  const [isAuth, setIsAuth] = usePersistentAuth('isLoggedIn', false)
+  const [open, setOpen] = useState(false)
 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleOpen = () => {
     if (!isAuth) {
-      try {
-        const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-        console.log('User signed in:', userCredential.user)
-        setIsAuth(true)
-      } catch (error) {
-        console.error('Error signing in:', error)
-      }
+      setOpen(true)
+    }
+    if (isAuth) {
+      handleLogout()
+    }
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth)
+      setIsAuth(false)
+      console.log('User signed out')
+    } catch (error) {
+      console.error('Error signing out:', error)
+    }
+  }
+
+  const handleLogin = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, username, password)
+      console.log('User signed in:', userCredential.user)
+      setIsAuth(true)
+      setOpen(false)
+    } catch (error) {
+      console.error('Error signing in:', error)
     }
   }
 
@@ -41,11 +56,68 @@ export const Authenticator = () => {
   console.log('isAuth:', isAuth)
 
   return (
-    <Tooltip title={handleLoginButtonLabel}>
-      <StyledIconButton onClick={handleClick} aria-label={handleLoginButtonLabel}>
-        {isAuth ? <LogoutIcon /> : <LoginIcon />}
-      </StyledIconButton>
-    </Tooltip>
+    <>
+      <Tooltip title={handleLoginButtonLabel}>
+        <StyledIconButton onClick={handleOpen} aria-label={handleLoginButtonLabel}>
+          {isAuth ? <LogoutIcon /> : <LoginIcon />}
+        </StyledIconButton>
+      </Tooltip>
+
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth='sm'>
+        <DialogTitle>
+          {BTN_LABEL_LOGIN}
+          <IconButton
+            aria-label={LABEL_CLOSE}
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+            }}>
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin='dense'
+            label='username'
+            type='text'
+            fullWidth
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault()
+                handleLogin()
+              }
+            }}
+          />
+          <TextField
+            margin='dense'
+            label='password'
+            type='password'
+            fullWidth
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault()
+                handleLogin()
+              }
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='secondary'>
+            {BTN_LABEL_CANCEL}
+          </Button>
+          <Button onClick={handleLogin} color='primary'>
+            {BTN_LABEL_OK}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 export default Authenticator
