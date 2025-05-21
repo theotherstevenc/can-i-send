12a439f9066df280ed88f0901a7b343f4ff80a0a
theotherstevenc <steven.winston.collins@gmail.com>
2025-05-21 09:59:58 -0700

    refactor: reorganize imports and enhance error handling

--- src/components/InputMarkupSettings.tsx
+++ src/components/InputMarkupSettings.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Checkbox, FormControlLabel } from '@mui/material'
-import { useAppContext } from '../context/AppContext'
-import { useEditorContext } from '../context/EditorContext'
-import { useEffect } from 'react'
-import { customMinifier } from '../utils/customMinifier'
-import { SETTINGS_CHECKBOX_LABEL_MINIFY, SETTINGS_CHECKBOX_LABEL_PREVENT_THREADING, SETTINGS_CHECKBOX_LABEL_WORD_WRAP } from '../utils/constants'
 import { db } from '../firebase'
+import { useEffect } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { Checkbox, FormControlLabel } from '@mui/material'
+import { useEditorContext } from '../context/EditorContext'
+import { customMinifier } from '../utils/customMinifier'
 import { updateFirestoreDoc } from '../utils/updateFirestoreDoc'
+import { SETTINGS_CHECKBOX_LABEL_MINIFY, SETTINGS_CHECKBOX_LABEL_PREVENT_THREADING, SETTINGS_CHECKBOX_LABEL_WORD_WRAP } from '../utils/constants'
 
 const InputMarkupSettings = () => {
   const { setHtml, html, setOriginalHtml, originalHtml } = useEditorContext()
@@ -28,12 +28,20 @@ const InputMarkupSettings = () => {
     // Find the setting object from the settings array that matches the name of the checkbox being toggled
     const setting = settings.find((setting) => setting.name === name)
 
+    // Update the UI first
     if (setting) {
       setting.setter(checked)
+    } else {
+      console.warn('No setting found for checkbox name: ' + name)
     }
 
     const firestoreObj = { [name]: checked }
-    updateFirestoreDoc(db, COLLECTION, DOCUMENT, firestoreObj)
+
+    try {
+      await updateFirestoreDoc(db, COLLECTION, DOCUMENT, firestoreObj)
+    } catch (error) {
+      console.error('Error updating settings: ', error)
+    }
   }
 
   const updateHtml = () => {
