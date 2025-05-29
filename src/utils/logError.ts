c5951f92af7105cb36149ef1b99be9b52e75e661
theotherstevenc <steven.winston.collins@gmail.com>
2025-05-29 16:06:46 -0700

    fix: adjust logError message format for clarity

--- src/utils/logError.ts
+++ src/utils/logError.ts
@@ -1,7 +1,7 @@
 export const logError = (message: string, file: string, error?: unknown) => {
   if (process.env.NODE_ENV === 'development') {
     const errorToLog = error ?? '[no error details provided]'
-    console.error(`[${file}] [${message}]`, errorToLog)
+    console.error(`[${message}] in [${file}]`, errorToLog)
   } else {
     console.error(`An error occurred. Please try again later.`)
   }
