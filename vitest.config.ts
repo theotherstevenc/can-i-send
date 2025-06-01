e7baf47be4930880d6187a5b09c9dfe215d72f56
theotherstevenc <steven.winston.collins@gmail.com>
2025-06-01 11:39:20 -0700

    config: update vitest configuration to include .tsx files and enhance coverage settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.ts'],
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    exclude: ['build/**'],
     environment: 'jsdom',
+    coverage: {
+      exclude: ['build/**', '*.config.*'],
+    },
   },
 })
