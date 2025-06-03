32c8a4bd8f90c2fb8a51447e4265652e23ca3bcf
theotherstevenc <steven.winston.collins@gmail.com>
2025-06-03 15:36:21 -0700

    chore: update dependencies and add vitest setup for testing-library

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config'
+
 export default defineConfig({
   test: {
     include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
     exclude: ['build/**'],
     environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
     coverage: {
       exclude: ['build/**', '*.config.*'],
     },
