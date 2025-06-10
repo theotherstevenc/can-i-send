a1bdc23b4a339078781c4806b03b3cd4b555f234
theotherstevenc <steven.winston.collins@gmail.com>
2025-06-10 12:22:35 -0700

    refactor: update EditorType to use constants for maintainability

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,3 @@
-export type EditorType = 'html' | 'text' | 'amp'
+import { EDITOR_OPTION_AMP, EDITOR_OPTION_HTML, EDITOR_OPTION_TEXT } from '../utils/constants'
+
+export type EditorType = typeof EDITOR_OPTION_HTML | typeof EDITOR_OPTION_TEXT | typeof EDITOR_OPTION_AMP
