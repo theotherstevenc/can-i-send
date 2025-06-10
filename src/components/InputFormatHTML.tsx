6a04e7bba8c90eecdfe40714587282b24131fb6a
theotherstevenc <steven.winston.collins@gmail.com>
2025-06-10 16:20:26 -0700

    feat: enhance InputFormatHTML component with minification check and CSS formatting

--- src/components/InputFormatHTML.tsx
+++ src/components/InputFormatHTML.tsx
@@ -1,21 +1,27 @@
 import FormatPaintIcon from '@mui/icons-material/FormatPaint'
+import { INPUT_FORMAT_HTML_LABEL } from '../utils/constants'
 import { useEditorContext } from '../context/EditorContext'
+import { useAppContext } from '../context/AppContext'
 import { StyledIconButton } from './InputIconButton'
 import { Tooltip } from '@mui/material'
 import { logError } from '../utils/logError'
 import prettier from 'prettier/standalone'
 import parserHtml from 'prettier/plugins/html'
-
-const InputFormatHTMLLabel = 'Format HTML'
+import parserPostcss from 'prettier/plugins/postcss'
 
 const InputFormatHTML = () => {
   const { html, setHtml } = useEditorContext()
+  const { isMinifyEnabled } = useAppContext()
 
   const handleClick = async () => {
+    if (isMinifyEnabled) {
+      logError('Formatting is disabled when minification is enabled', 'InputFormatHTML')
+      return
+    }
     try {
       const formattedHtml = await prettier.format(html, {
         parser: 'html',
-        plugins: [parserHtml],
+        plugins: [parserHtml, parserPostcss],
         printWidth: 120,
         tabWidth: 1,
         useTabs: false,
@@ -30,8 +36,8 @@ const InputFormatHTML = () => {
   }
   return (
     <>
-      <Tooltip title={InputFormatHTMLLabel}>
-        <StyledIconButton onClick={handleClick} aria-label={InputFormatHTMLLabel}>
+      <Tooltip title={INPUT_FORMAT_HTML_LABEL}>
+        <StyledIconButton onClick={handleClick} aria-label={INPUT_FORMAT_HTML_LABEL}>
           <FormatPaintIcon />
         </StyledIconButton>
       </Tooltip>
