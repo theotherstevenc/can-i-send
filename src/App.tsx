28208cfaff6ac788ebed38fb171e59214b9d5313
theotherstevenc <steven.winston.collins@gmail.com>
2025-06-16 15:41:01 -0700

    refactor: reorganize imports and simplify component

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,61 @@
 import './App.css'
+import { EditorProvider } from './context/EditorContext'
 import { Box, CssBaseline, ThemeProvider } from '@mui/material'
+import { editorActionsStyles, inputActionsStyles, inputConfigStyles, inputSenderSettingsStyles } from './styles/global.styles'
+import Authenticator from './components/Authenticator'
+import EditorContainer from './components/EditorContainer'
 import EditorSelectorButtons from './components/EditorSelectorButtons'
 import EditorSendButton from './components/EditorSendButton'
-import InputEmailListSubjectLine from './components/InputEmailListSubjectLine'
+import FontSizeControls from './components/FontSizeControls'
 import InputCreateNewFile from './components/InputCreateNewFile'
+import InputDeleteFile from './components/InputDeleteFile'
+import InputEmailListSubjectLine from './components/InputEmailListSubjectLine'
 import InputFileUpload from './components/InputFileUpload'
+import InputFormatHTML from './components/InputFormatHTML'
+import InputLockFile from './components/InputLockFile'
 import InputMarkupSettings from './components/InputMarkupSettings'
 import InputSenderSettings from './components/InputSenderSettings'
-import EditorContainer from './components/EditorContainer'
-import { EditorProvider } from './context/EditorContext'
-import InputDeleteFile from './components/InputDeleteFile'
-import InputUpdateFiles from './components/InputUpdateFiles'
-import InputToggleWorkingFiles from './components/InputToggleWorkingFiles'
 import InputToggleEditorTheme from './components/InputToggleEditorTheme'
-import { editorActionsStyles, inputActionsStyles, inputConfigStyles, inputSenderSettingsStyles } from './styles/global.styles'
+import InputToggleWorkingFiles from './components/InputToggleWorkingFiles'
+import InputUpdateFiles from './components/InputUpdateFiles'
 import usePreferredTheme from './utils/usePreferredTheme'
-import Authenticator from './components/Authenticator'
-import InputLockFile from './components/InputLockFile'
-import FontSizeControls from './components/FontSizeControls'
-import InputFormatHTML from './components/InputFormatHTML'
 
 function App() {
   const theme = usePreferredTheme()
 
   return (
-    <>
-      <EditorProvider>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Box sx={inputConfigStyles}>
-            <Box>
-              <InputMarkupSettings />
-            </Box>
-            <Box sx={inputSenderSettingsStyles}>
-              <InputSenderSettings />
-            </Box>
-            <Box sx={inputActionsStyles}>
-              <InputFormatHTML />
-              <FontSizeControls />
-              <InputLockFile />
-              <InputToggleEditorTheme />
-              <InputToggleWorkingFiles />
-              <InputCreateNewFile />
-              <InputUpdateFiles />
-              <InputDeleteFile />
-              <InputFileUpload />
-              <Authenticator />
-            </Box>
+    <EditorProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box sx={inputConfigStyles}>
+          <Box>
+            <InputMarkupSettings />
           </Box>
+          <Box sx={inputSenderSettingsStyles}>
+            <InputSenderSettings />
+          </Box>
+          <Box sx={inputActionsStyles}>
+            <InputFormatHTML />
+            <FontSizeControls />
+            <InputLockFile />
+            <InputToggleEditorTheme />
+            <InputToggleWorkingFiles />
+            <InputCreateNewFile />
+            <InputUpdateFiles />
+            <InputDeleteFile />
+            <InputFileUpload />
+            <Authenticator />
+          </Box>
+        </Box>
 
-          <Box sx={editorActionsStyles}>
-            <EditorSelectorButtons />
-            <InputEmailListSubjectLine />
-            <EditorSendButton />
-          </Box>
-          <EditorContainer />
-        </ThemeProvider>
-      </EditorProvider>
-    </>
+        <Box sx={editorActionsStyles}>
+          <EditorSelectorButtons />
+          <InputEmailListSubjectLine />
+          <EditorSendButton />
+        </Box>
+        <EditorContainer />
+      </ThemeProvider>
+    </EditorProvider>
   )
 }
 
