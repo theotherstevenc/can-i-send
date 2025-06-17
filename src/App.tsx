67cc5b2028b8f8ac6c4338295f7b384aabfffae4
theotherstevenc <steven.winston.collins@gmail.com>
2025-06-17 10:12:09 -0700

    refactor: consolidate component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import './App.css'
 import { EditorProvider } from './context/EditorContext'
+import { usePreferredTheme } from './utils/usePreferredTheme'
 import { Box, CssBaseline, ThemeProvider } from '@mui/material'
 import { editorActionsStyles, inputActionsStyles, inputConfigStyles, inputSenderSettingsStyles } from './styles/global.styles'
-import Authenticator from './components/Authenticator'
-import EditorContainer from './components/EditorContainer'
-import EditorSelectorButtons from './components/EditorSelectorButtons'
-import EditorSendButton from './components/EditorSendButton'
-import FontSizeControls from './components/FontSizeControls'
-import InputCreateNewFile from './components/InputCreateNewFile'
-import InputDeleteFile from './components/InputDeleteFile'
-import InputEmailListSubjectLine from './components/InputEmailListSubjectLine'
-import InputFileUpload from './components/InputFileUpload'
-import InputFormatHTML from './components/InputFormatHTML'
-import InputLockFile from './components/InputLockFile'
-import InputMarkupSettings from './components/InputMarkupSettings'
-import InputSenderSettings from './components/InputSenderSettings'
-import InputToggleEditorTheme from './components/InputToggleEditorTheme'
-import InputToggleWorkingFiles from './components/InputToggleWorkingFiles'
-import InputUpdateFiles from './components/InputUpdateFiles'
-import usePreferredTheme from './utils/usePreferredTheme'
+
+import {
+  Authenticator,
+  EditorContainer,
+  EditorSelectorButtons,
+  EditorSendButton,
+  FontSizeControls,
+  InputCreateNewFile,
+  InputDeleteFile,
+  InputEmailListSubjectLine,
+  InputFileUpload,
+  InputFormatHTML,
+  InputLockFile,
+  InputMarkupSettings,
+  InputSenderSettings,
+  InputToggleEditorTheme,
+  InputToggleWorkingFiles,
+  InputUpdateFiles,
+} from './components'
 
 function App() {
   const theme = usePreferredTheme()
